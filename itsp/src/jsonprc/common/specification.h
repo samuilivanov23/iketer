7d7bcacfa6dd0f251b69d3999cec80cb59cc3ec5
samuilivanov23 <samuil2001ivanov@gmail.com>
2022-11-12 19:43:41 +0200

    (2022-11-12) Implementing GetProcedure, GetPositionalParameters(), GetNamedParameters(), GetProcedureName, toJsonType

--- itsp/src/jsonprc/common/specification.h
+++ itsp/src/jsonprc/common/specification.h
@@ -4,6 +4,7 @@
 #define KEY_SPEC_PROCEDURE_NAME "name"
 #define KEY_SPEC_PROCEDURE_METHOD "method"
 #define KEY_SPEC_PROCEDURE_NOTIFICATION "notification"
+#define KEY_SPEC_PROCEDURE_PARAMETERS "parameters"
 #define KEY_SPEC_RETURN_TYPE "returns"
 
 namespace itsp
@@ -12,7 +13,7 @@ namespace itsp
     typedef enum { RPC_METHOD, RPC_NOTIFICATION };
 
     //Defines all valid and processable types of this protocol
-    enum jsontype_t { JSON_STRING = 1, JSON_BOOLEAN = 2, JSON_INTEGER = 3, JSON_REAL = 4, JSON_OBJECT = 5, JSON_ARRAY = 6, JSON_NUMERIC = 7 }
+    enum jsontype_t { JSON_STRING = 1, JSON_BOOLEAN = 2, JSON_INTEGER = 3, JSON_UINTEGER = 4, JSON_REAL = 5, JSON_OBJECT = 6, JSON_ARRAY = 7, JSON_NUMERIC = 8 }
 }
 
 
--- itsp/src/jsonprc/common/specificationparser.cpp
+++ itsp/src/jsonprc/common/specificationparser.cpp
@@ -48,3 +48,105 @@ vector<Procedure> SpecificationParser::GetProceduresFromString( const string &sp
 }
 
 
+void SpecificationParser::GetFileContent( const std::string &fileName, std::string &target )
+{
+    ifstream config( fileName.c_str() );
+
+    if( config )
+    {
+        config.open( fileName.c_str(), iostream::in );
+        target.assign( (std::istreambuf_iterator<char>( config )), ( std::istreambuf_iterator<char>() ) );
+    }
+    else
+    {
+        throw JsonRpcException( Errors::ERROR_SERVER_PROCEDURE_SPECIFICATION_NOT_FOUND, fileName );
+    }
+}
+
+vector<Procedure> SpecificationParser::GetProcedure( Json::Value &procedureSignature, Procedure resultProcedure )
+{
+    if( procedureSignature.isObject() && !GetProcedureName( procedureSignature ).isEmpty() )
+    {
+        resultProcedure.SetProcedureName( GetProcedureName( procedureSignature ) );
+        if( procedureSignature.isMember( KEY_SPEC_RETURN_TYPE ) )
+        {
+            resultProcedure.SetProcedureType( RPC_METHOD );
+            resultProcedure.SetReturnType( toJsonType( procedureSignature[KEY_SPEC_RETURN_TYPE] ) );
+        }
+        else
+        {
+            resultProcedure.SetProcedureType( RPC_NOTIFICATION );
+        }
+
+        if( procedureSignature.isMember( KEY_SPEC_PROCEDURE_PARAMETERS ) )
+        {
+            if( procedureSignature[KEY_SPEC_PROCEDURE_PARAMETERS].isObject() || procedureSignature[KEY_SPEC_PROCEDURE_PARAMETERS].isArray() )
+            {
+                if( procedureSignature[KEY_SPEC_PROCEDURE_PARAMETERS].isObject() )
+                {
+                    resultProcedure.SetParameterDeclarationType( PARAMS_BY_NAME );
+                    GetNamedParameters( procedureSignature, resultProcedure );
+                }
+                else if ( procedureSignature[KEY_SPEC_PROCEDURE_PARAMETERS].isArray() )
+                {
+                    resultProcedure.SetParameterDeclarationType( PARAMS_BY_POSITION );
+                    GetPositionalParameters( procedureSignature, resultProcedures );
+                }
+            }
+            else
+            {
+                throw JsonRpcException( Errors::ERROR_SERVER_PROCEDURE_SPECIFICATION_SYNTAX, "||| Invalid signature types in fields ||| " + procedureSignature.toStyledString() );
+            }
+        }
+    }
+    else
+    {
+        throw JsonRpcException( Errors::ERROR_SERVER_PROCEDURE_SPECIFICATION_SYNTAX, " ||| procedure declaration must contain name and parameters! IT DOES NOT" );
+    }
+}
+
+jsontype_t SpecicifationParser::toJsonType( Json::Value &value )
+{
+    jsontype_t resultJson;
+    switch( value.type() )
+    {
+        case Json::uintValue:       resultJson = JSON_UINTEGER; break;
+        case Json::intValue:        resultJson = JSON_INTEGER;  break;
+        case Json::realValue:       resultJson = JSON_REAL;     break;
+        case Json::stringValue:     resultJson = JSON_STRING;   break;
+        case Json::booleanValue:    resultJson = JSON_BOOLEAN;  break;
+        case Json::arrayValue:      resultJson = JSON_ARRAY;    break;
+        case Json::objectValue:     resultJson = JSON_OBJECT;   break;
+        default: throw JsonRpcException( Errors::ERROR_SERVER_PROCEDURE_SPECIFICATION_SYNTAX, "||| Unknown parameter type ||| " + value.toStyledString() );
+    }
+
+    return resultJson;
+}
+
+void SpecificationParser::GetPositionalParameters( Json::Value &value, Procedure &resultProcedure )
+{
+    for( uint16_t i = 0; i < value[KEY_SPEC_PROCEDURE_PARAMETERS].size(); i++ )
+    {
+        stringstream parameterName;
+        parameterName << "parameter" << std::setfill('0') << std::setw(2) << (i + 1);
+        resultProcedure.AddParameter( parameterName.str(), toJsonType( value[KEY_SPEC_PROCEDURE_PARAMETERS][i] ) );
+    }
+}
+
+void SpecificationParser::GetNamedParameters( Json::Value &value, Procedure &resultProcedure )
+{
+    vector<string> parameters = value[KEY_SPEC_PROCEDURE_PARAMETERS].getMemberNames();
+    for( uint16_t i = 0; i < parameters.size(); i++ )
+    {
+        resultProcedure.AddParameter( parameters.at(i), toJsonType( value[KEY_SPEC_PROCEDURE_PARAMETERS][parameters.at(i)] ) );
+    }
+}
+
+std::string SpecificationParser::GetProcedureName( Json::Value &procedureSignature )
+{
+    if( procedureSignature[KEY_SPEC_PROCEDURE_NAME].isString() )            { return procedureSignature[KEY_SPEC_PROCEDURE_NAME].asString(); }
+    if( procedureSignature[KEY_SPEC_PROCEDURE_METHOD].isString() )          { return procedureSignature[KEY_SPEC_PROCEDURE_METHOD].asString(); }
+    if( procedureSignature[KEY_SPEC_PROCEDURE_NOTIFICATION].isString() )    { return procedureSignature[KEY_SPEC_PROCEDURE_NOTIFICATION].asString(); }
+
+    return "";
+}
