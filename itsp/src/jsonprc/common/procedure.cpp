16b30adca750fad91d280048dff8f79126e8ff1d
samuilivanov23 <samuil2001ivanov@gmail.com>
2022-11-13 18:00:16 +0200

    (2022-11-13) Adding validation for Named and Positional params of a procedure

--- itsp/src/jsonprc/common/procedure.cpp
+++ itsp/src/jsonprc/common/procedure.cpp
@@ -84,4 +84,51 @@ void Procedure::AddParameter( const std::string &procedureName, jsontype_t param
     this->procedureParametersPosition.push_babk( parameterType );
 }
 
+bool Procedure::ValidateNamedParameters( const Json::Value &parameters ) const
+{
+    bool areParametersValid = parameters.isObject() || parameters.isNull();
+    for( map<std::string, jsontype_t>::const_iterator i = this->procedureParametersName.begin(); isValid && i.procedureParametersName.end(); ++i )
+    {
+        if( !parameters.isMember( i->first ) )
+        {
+            areParametersValid = false;
+        }
+        else
+        {
+            areParametersValid = this->ValidateSingleParameter( i->second, parameters[i->first] );
+        }
+    }
+
+    return areParametersValid;
+}
+
+bool Procedure::ValidatePositionalParameters( const Json::Value &parameters ) const
+{
+    bool areParametersValid = true;
+    if( parameters.size() != this->procedureParametersPosition.size() ) { return false; }
+
+    for( uint16_t i = 0; i < parameters.size(); i++) 
+    {
+        areParametersValid = this->ValidateSingleParameter( this->procedureParametersPosition.at(i), parameters[i] );  
+    }
+
+    return areParametersValid;
+}
+
+bool Procedure::ValidateSingleParameter( jsontype_t expectedParameterType, const Json::Value &parameterValue ) const
+{
+    bool isParameterValid = true;
+    switch( expectedParameterType )
+    {
+        case JSON_INTEGER:  if( !parameterValue.isInt() )       isParameterValid = false; break;
+        case JSON_REAL:     if( !parameterValue.isDouble() )    isParameterValid = false; break;
+        case JSON_NUMERIC:  if( !parameterValue.isNumeric() )   isParameterValid = false; break;
+        case JSON_STRING:   if( !parameterValue.isString() )    isParameterValid = false; break;
+        case JSON_BOOLEAN:  if( !parameterValue.isBool() )      isParameterValid = false; break;
+        case JSON_OBJECT:   if( !parameterValue.isObject() )    isParameterValid = false; break;
+        case JSON_ARRAY:    if( !parameterValue.isArray() )     isParameterValid = false; break;
+    }
+
+    return isParameterValid;
+}
 
